e3206ff9bfdbd3798b80c29253f63f83d8cf5fe1
SanjitGorai <sanjitprasadgorai56@gmail.com>
2023-06-11 00:12:27 +0530

    whckckc

--- SeleniumDemo/src/test/java/com/actitime/test1/LoginTest.java
+++ SeleniumDemo/src/test/java/com/actitime/test1/LoginTest.java
@@ -21,7 +21,7 @@ public class LoginTest extends BaseTest {
 	public void loginValidation() throws Exception{
 		try {
 				
-			//Sajit
+			//Sajit prasad gorai
 			
 			loginPage = new LoginPage(driver);
 			eventUtils= new EventUtils(driver);
